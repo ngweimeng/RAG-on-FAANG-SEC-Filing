c7702660105307f99777daf134c307248d650180
Ng Wei Meng <40542706+ngweimeng@users.noreply.github.com>
2024-04-09 22:15:17 +0200

    Create db_build.py

---
+++ db_build.py
@@ -0,0 +1,26 @@
+# =========================
+#  Module: Vector DB Build
+# =========================
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+
+# Build vector database
+def run_db_build():
+    loader = DirectoryLoader('data/',
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+                                                   chunk_overlap=50)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
+
+    vectorstore = FAISS.from_documents(texts, embeddings)
+    vectorstore.save_local('vectorstore/db_faiss')
+
+if __name__ == "__main__":
+    run_db_build()

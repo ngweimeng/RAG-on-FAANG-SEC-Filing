860bd6ca181accf92ff63bde246a3510b270ecaf
Ng Wei Meng <40542706+ngweimeng@users.noreply.github.com>
2024-04-12 13:40:12 +0200

    Update prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -4,7 +4,7 @@
 ===========================================
 '''
 
-qa_template = """You are a trained accountant. Use the following pieces of information to answer the user's question.
+qa_template = """Use the following pieces of information to answer the user's question.
 If you don't know the answer, say that you do not know. Do not make up an answer.
 
 Context: {context}

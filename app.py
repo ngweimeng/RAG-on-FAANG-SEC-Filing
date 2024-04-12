7070ac0161184a386ba9020238b5d64f55997bb4
Ng Wei Meng <40542706+ngweimeng@users.noreply.github.com>
2024-04-12 17:41:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,28 @@ import timeit
 def main():
     st.title("Analyzing 10-K Filings submitted in 2023 for FAANG Companies: A Retrieval Augmented Generation (RAG) Approach")
 
-    # Sidebar content
+    # Sidebar content with explanations for first-time users
     st.sidebar.header('About the App')
     st.sidebar.write("""
-        This application uses a Retrieval Augmented Generation (RAG) approach powered by OpenAI's "gpt-3.5-turbo-instruct" to analyze the latest 10-K filings of FAANG companies. It provides insights into financial metrics and business performance.
+    **Overview:**
+    This application uses a Retrieval Augmented Generation (RAG) approach, powered by OpenAI's "gpt-3.5-turbo-instruct",
+    to analyze the latest 10-K filings (i.e 2023 Annual Financial Report) from FAANG companies. It provides insights into financial metrics and business performance.
 
-        **Why RAG?**
-        RAG combines retrieval-based and generative NLP approaches, ensuring data privacy, compliance with residency laws, and customization for proprietary needs.
+    **Why Use RAG?**
+    - **Data Privacy and Customization:** RAG allows organizations to analyze proprietary information securely within their own systems, reducing the risk of exposing sensitive data.
+    - **Up-to-Date Insights:** Unlike GPT models which may not be trained on the most recent data, RAG utilizes the latest documents directly. For example, if analyzing Q4 results of a FAANG company released in early 2024, RAG would use this fresh information directly from the source, whereas GPT models would rely on data only up to their last training cut-off.
+    - **Compliance with Data Residency Laws:** Ensures that data handling complies with local regulations by processing data within required jurisdictions.
 
-        **Note on Usage:**
-        The model supports up to 4097 tokens. Ensure your queries are concise and well-structured to avoid exceeding this limit.
+    **Getting Started:**
+    1. **Input Your Query:** Use the input box to type your financial query.
+    2. **Submit Your Query:** Click 'Analyze Query' to see results.
+    3. **View Results:** Read the insights and source documents provided.
 
-        **Feedback and Contributions:**
-        For feedback or contributions, please interact through the Streamlit app's feedback system or directly via GitHub issues.
+    **Note on Query Length:**
+    - The model can handle up to 4097 tokens. Keep queries concise to avoid errors.
+
+    **Feedback and Contributions:**
+    - Feedback is invaluable. Report issues and contribute via GitHub @ngweimeng.
     """)
 
     # User input for custom query
@@ -55,4 +64,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 

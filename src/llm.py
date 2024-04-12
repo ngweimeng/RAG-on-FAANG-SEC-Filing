f4ab4438a199a769261756063fd8f3157e29b249
Ng Wei Meng <40542706+ngweimeng@users.noreply.github.com>
2024-04-12 13:50:21 +0200

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -7,7 +7,7 @@ from langchain.llms import OpenAI
 import streamlit as st
 
 def build_llm():
-    llm = OpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=st.secrets["openai_api_key"])
+    llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0, openai_api_key=st.secrets["openai_api_key"])
     return llm
 
 
